d463d523bd119a51a276cfb71f1bd9c4391663ad
Aoi <ht.so.ik0102@gmail.com>
2022-02-19 19:56:12 +0700

    create config file

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// localhost
+// export const watchdexBaseaddress = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
+// export const nftfractionRepositoryaddress = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6"
+// export const erc721mockaddress = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318"
+
+// RinkedBy
+export const watchdexBaseaddress = "0xE3fb56F3BbF357fE23454491D8e663C2E36B217D"
+export const nftfractionRepositoryaddress = "0xc0f422c3203c02067860cb19EA3DA1A3D2465462"
+export const erc721mockaddress = "0x1D5d0e80f28aB57Ba6011cE3dde8E7e77446F21B"
+
+// Old Ropsten contract address
+// const CONTRACT_ADDRESS = "0x1EF88F4ED90c7E56305b054ddB1cafB9458D3419";
