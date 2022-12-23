079e6f1f13c7dacc415e78b4323a3f161fb6bce4
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-23 23:51:27 +0700

    Add new function

--- ts-client/nameservice.nameservice/module.ts
+++ ts-client/nameservice.nameservice/module.ts
@@ -7,12 +7,18 @@ import { msgTypes } from './registry';
 import { IgniteClient } from "../client"
 import { MissingWalletError } from "../helpers"
 import { Api } from "./rest";
+import { MsgBuyName } from "./types/nameservice/nameservice/tx";
 import { MsgDeleteName } from "./types/nameservice/nameservice/tx";
 import { MsgSetName } from "./types/nameservice/nameservice/tx";
-import { MsgBuyName } from "./types/nameservice/nameservice/tx";
 
 
-export { MsgDeleteName, MsgSetName, MsgBuyName };
+export { MsgBuyName, MsgDeleteName, MsgSetName };
+
+type sendMsgBuyNameParams = {
+  value: MsgBuyName,
+  fee?: StdFee,
+  memo?: string
+};
 
 type sendMsgDeleteNameParams = {
   value: MsgDeleteName,
@@ -26,12 +32,10 @@ type sendMsgSetNameParams = {
   memo?: string
 };
 
-type sendMsgBuyNameParams = {
-  value: MsgBuyName,
-  fee?: StdFee,
-  memo?: string
-};
 
+type msgBuyNameParams = {
+  value: MsgBuyName,
+};
 
 type msgDeleteNameParams = {
   value: MsgDeleteName,
@@ -39,10 +43,6 @@ type msgDeleteNameParams = {
 
 type msgSetNameParams = {
   value: MsgSetName,
-};
-
-type msgBuyNameParams = {
-  value: MsgBuyName,
 };
 
 
@@ -62,6 +62,20 @@ interface TxClientOptions {
 export const txClient = ({ signer, prefix, addr }: TxClientOptions = { addr: "http://localhost:26657", prefix: "cosmos" }) => {
 
   return {
+		
+		async sendMsgBuyName({ value, fee, memo }: sendMsgBuyNameParams): Promise<DeliverTxResponse> {
+			if (!signer) {
+					throw new Error('TxClient:sendMsgBuyName: Unable to sign Tx. Signer is not present.')
+			}
+			try {			
+				const { address } = (await signer.getAccounts())[0]; 
+				const signingClient = await SigningStargateClient.connectWithSigner(addr,signer,{registry, prefix});
+				let msg = this.msgBuyName({ value: MsgBuyName.fromPartial(value) })
+				return await signingClient.signAndBroadcast(address, [msg], fee ? fee : defaultFee, memo)
+			} catch (e: any) {
+				throw new Error('TxClient:sendMsgBuyName: Could not broadcast Tx: '+ e.message)
+			}
+		},
 		
 		async sendMsgDeleteName({ value, fee, memo }: sendMsgDeleteNameParams): Promise<DeliverTxResponse> {
 			if (!signer) {
@@ -91,20 +105,14 @@ export const txClient = ({ signer, prefix, addr }: TxClientOptions = { addr: "ht
 			}
 		},
 		
-		async sendMsgBuyName({ value, fee, memo }: sendMsgBuyNameParams): Promise<DeliverTxResponse> {
-			if (!signer) {
-					throw new Error('TxClient:sendMsgBuyName: Unable to sign Tx. Signer is not present.')
-			}
-			try {			
-				const { address } = (await signer.getAccounts())[0]; 
-				const signingClient = await SigningStargateClient.connectWithSigner(addr,signer,{registry, prefix});
-				let msg = this.msgBuyName({ value: MsgBuyName.fromPartial(value) })
-				return await signingClient.signAndBroadcast(address, [msg], fee ? fee : defaultFee, memo)
+		
+		msgBuyName({ value }: msgBuyNameParams): EncodeObject {
+			try {
+				return { typeUrl: "/nameservice.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( value ) }  
 			} catch (e: any) {
-				throw new Error('TxClient:sendMsgBuyName: Could not broadcast Tx: '+ e.message)
+				throw new Error('TxClient:MsgBuyName: Could not create message: ' + e.message)
 			}
 		},
-		
 		
 		msgDeleteName({ value }: msgDeleteNameParams): EncodeObject {
 			try {
@@ -119,14 +127,6 @@ export const txClient = ({ signer, prefix, addr }: TxClientOptions = { addr: "ht
 				return { typeUrl: "/nameservice.nameservice.MsgSetName", value: MsgSetName.fromPartial( value ) }  
 			} catch (e: any) {
 				throw new Error('TxClient:MsgSetName: Could not create message: ' + e.message)
-			}
-		},
-		
-		msgBuyName({ value }: msgBuyNameParams): EncodeObject {
-			try {
-				return { typeUrl: "/nameservice.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( value ) }  
-			} catch (e: any) {
-				throw new Error('TxClient:MsgBuyName: Could not create message: ' + e.message)
 			}
 		},
 		
--- x/nameservice/keeper/msg_server_buy_name.go
+++ x/nameservice/keeper/msg_server_buy_name.go
@@ -3,15 +3,58 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"nameservice/x/nameservice/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) BuyName(goCtx context.Context, msg *types.MsgBuyName) (*types.MsgBuyNameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Try getting a name from the store
+	whois, isFound := k.GetWhois(ctx, msg.Name)
 
+	// Set the price at which the name has to be bought if it didn't have an owner before
+	minPrice := sdk.Coins{sdk.NewInt64Coin("token", 10)}
+
+	// Convert price and bid strings to sdk.Coins
+	price, _ := sdk.ParseCoinsNormalized(whois.Price)
+	bid, _ := sdk.ParseCoinsNormalized(msg.Bid)
+
+	// Convert owner and buyer address strings to sdk.AccAddress
+	owner, _ := sdk.AccAddressFromBech32(whois.Owner)
+	buyer, _ := sdk.AccAddressFromBech32(msg.Creator)
+
+	if isFound {
+		if price.IsAllGT(bid) {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Bid is not high enough")
+		}
+
+		err := k.bankKeeper.SendCoins(ctx, buyer, owner, bid)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		if minPrice.IsAllGT(bid) {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Bid is less than min amount")
+		}
+
+		err := k.bankKeeper.SendCoinsFromAccountToModule(ctx, buyer, types.ModuleName, bid)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	newWhois := types.Whois{
+		Index: msg.Name,
+		Name:  msg.Name,
+		Value: whois.Value,
+		Price: bid.String(),
+		Owner: buyer.String(),
+	}
+
+	k.SetWhois(ctx, newWhois)
 	return &types.MsgBuyNameResponse{}, nil
 }
