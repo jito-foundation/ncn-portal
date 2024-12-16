1f5e80836f401f7ec33fafba2d5195c99fa7d0fc
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2024-12-17 05:37:05 +0900

    Feature: Add Solana wallet button (#7)
    * feat: wallet
    
    * feat: wallet installed

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,16 +1,18 @@
-'use client'
-import { Suspense } from 'react'
-import { Flex } from '@radix-ui/themes'
-import { Chat, ChatContext, ChatSideBar, useChatHook } from '@/components'
-import PersonaModal from './PersonaModal'
-import PersonaPanel from './PersonaPanel'
+"use client";
+import { Suspense } from "react";
+import { Flex } from "@radix-ui/themes";
+import { Chat, ChatContext, ChatSideBar, useChatHook } from "@/components";
+import PersonaModal from "./PersonaModal";
+import PersonaPanel from "./PersonaPanel";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 
 const ChatProvider = () => {
-  const provider = useChatHook()
+  const provider = useChatHook();
 
   return (
     <ChatContext.Provider value={provider}>
-      <Flex style={{ height: 'calc(100% - 56px)' }} className="relative">
+      <Flex style={{ height: "calc(100% - 56px)" }} className="relative">
         <ChatSideBar />
         <div className="flex-1 relative">
           <Chat ref={provider.chatRef} />
@@ -19,15 +21,26 @@ const ChatProvider = () => {
       </Flex>
       <PersonaModal />
     </ChatContext.Provider>
-  )
-}
+  );
+};
 
 const ChatPage = () => {
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
   return (
     <Suspense>
-      <ChatProvider />
+      {publicKey ? (
+        <ChatProvider />
+      ) : (
+        <main className="flex items-center justify-center min-h-screen">
+          <div className="border hover:border-slate-900 rounded">
+            <WalletMultiButton style={{}} />
+          </div>
+        </main>
+      )}
     </Suspense>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,14 @@ import { Header } from '@/components/Header'
 import ThemesProvider from '@/providers/ThemesProvider'
 import '@/styles/globals.scss'
 import '@/styles/theme-config.css'
+import AppWalletProvider from '@/components/AppWalletProvider'
 
 export const metadata = {
   title: {
     default: 'NCN Portal',
-    template: `%s - ChatGPT Lite`
+    template: `%s - Claude Lite`
   },
-  description: 'AI assistant powered by ChatGPT',
+  description: 'AI assistant powered by Claude',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
@@ -22,11 +23,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <ThemesProvider>
-          <Header />
-          {children}
-          <Toaster />
-        </ThemesProvider>
+        <AppWalletProvider>
+          <ThemesProvider>
+            <Header />
+            {children}
+            <Toaster />
+          </ThemesProvider>
+        </AppWalletProvider>
         <Analytics />
       </body>
     </html>
