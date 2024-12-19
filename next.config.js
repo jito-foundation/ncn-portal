e82c55d07634093fe09a70858612efdd01c52734
aoikurokawa <ht.so.ik0102@gmail.com>
2024-12-20 06:25:50 +0900

    fix: remove unused page

--- next.config.js
+++ next.config.js
@@ -1,25 +1,23 @@
-require('path')
-
 const nextConfig = {
-  output: 'standalone',
-  sassOptions: {
-    // includePaths: [path.join(__dirname, 'styles')]
-  },
-  reactStrictMode: false,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/',
-        permanent: true
-      }
-    ]
-  },
-  logging: {
-    fetches: {
-      fullUrl: true
-    }
-  }
+  // output: 'standalone',
+  // sassOptions: {
+  //   // includePaths: [path.join(__dirname, 'styles')]
+  // },
+  // reactStrictMode: false,
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/',
+  //       permanent: true
+  //     }
+  //   ]
+  // },
+  // logging: {
+  //   fetches: {
+  //     fullUrl: true
+  //   }
+  // }
 }
 
 module.exports = nextConfig
