b3abc183578d5c121e6138b70a0924abcfa9c08b
aoikurokawa <ht.so.ik0102@gmail.com>
2024-12-20 06:36:49 +0900

    fix: remove unused page

--- next.config.js
+++ next.config.js
@@ -1,23 +1,21 @@
 const nextConfig = {
-  // output: 'standalone',
-  // sassOptions: {
-  //   // includePaths: [path.join(__dirname, 'styles')]
-  // },
-  // reactStrictMode: false,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/',
-  //       permanent: true
-  //     }
-  //   ]
-  // },
-  // logging: {
-  //   fetches: {
-  //     fullUrl: true
-  //   }
-  // }
+  output: 'standalone',
+  sassOptions: {},
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+        permanent: true
+      }
+    ]
+  },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  }
 }
 
 module.exports = nextConfig
