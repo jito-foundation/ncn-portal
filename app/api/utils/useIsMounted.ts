3e3f529b85846024beda002a0678387479968b23
aoikurokawa <ht.so.ik0102@gmail.com>
2024-12-19 21:26:58 +0900

    feat: update

---
+++ app/api/utils/useIsMounted.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+
+export default function useIsMounted() {
+  const [mounted, setMounted] = useState(false);
+
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+
+  return mounted;
+}
---
+++ hooks/useRequireAuth.ts
@@ -0,0 +1,15 @@
+// hooks/useRequireAuth.js
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "../components/context/AuthContext";
+
+export const useRequireAuth = () => {
+    const { isAuthenticated } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            router.push("/login");
+        }
+    }, [isAuthenticated, router]);
+};
