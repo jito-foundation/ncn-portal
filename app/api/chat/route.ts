8386f1191d0a0f9ef7698f520b3f15f5ac978910
aoikurokawa <ht.so.ik0102@gmail.com>
2024-12-27 19:19:26 +0900

    fix: clients

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: NextRequest) {
 
 const getApiConfig = () => {
   const apiUrl =
-    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1` ||
     "https://seal-app-m2hhp.ondigitalocean.app/api/v1";
 
   return { apiUrl };
