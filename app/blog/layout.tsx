396057244892c0cd615775583497607cc9a8aacb
aoikurokawa <ht.so.ik0102@gmail.com>
2025-02-28 08:11:12 +0900

    fix: background, height

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,22 +1,25 @@
-import { Metadata } from "next";
-import React from "react";
+import React from 'react';
+import { Metadata } from 'next';
+import './blog.css';
 
 export const metadata: Metadata = {
   title: {
-    default: "NCN Blog",
-    template: "%s | NCN Blog",
+    default: 'NCN Blog',
+    template: '%s | NCN Blog'
   },
-  description: "The official blog for the NCN Project",
+  description: 'The official blog for the NCN Project'
 };
 
 export default function BlogLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className="">
-      <div className="blog-container">{children}</div>
+    <section className="bg-black">
+      <div className="blog-container">
+        {children}
+      </div>
     </section>
   );
 }
