a6448d94b69d419c48e6c5523d58d9c39417b283
aoikurokawa <ht.so.ik0102@gmail.com>
2025-02-28 08:11:36 +0900

    fix: format

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import { Metadata } from 'next';
-import './blog.css';
+import { Metadata } from "next";
+import React from "react";
+
+import "./blog.css";
 
 export const metadata: Metadata = {
   title: {
-    default: 'NCN Blog',
-    template: '%s | NCN Blog'
+    default: "NCN Blog",
+    template: "%s | NCN Blog",
   },
-  description: 'The official blog for the NCN Project'
+  description: "The official blog for the NCN Project",
 };
 
 export default function BlogLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <section className="bg-black">
-      <div className="blog-container">
-        {children}
-      </div>
+      <div className="blog-container">{children}</div>
     </section>
   );
 }
