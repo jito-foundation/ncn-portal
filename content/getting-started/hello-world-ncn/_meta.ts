0d5f016863adbf1b5cc2dc120bf1a8d725e3948e
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-03-09 15:32:57 +0900

    Hello World NCN, Interact with Jito Restaking (#62)
    * fix: update
    
    * fix: update

---
+++ content/getting-started/hello-world-ncn/_meta.ts
@@ -0,0 +1,15 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  "getting-started": {
+    title: "Getting Started",
+  },
+  "jito-restaking": {
+    title: "Jito Restaking",
+  },
+  "ncn-on-chain": {
+    title: "NCN On-Chain",
+  },
+};
+
+export default meta;
